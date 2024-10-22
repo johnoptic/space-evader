9ff75180cc7a5cff63bcb1b4fe374710ea851093
John <johnoptic@proton.me>
2024-10-22 23:57:25 +0100

    update player to move along x axis

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,6 @@ canvas.height = innerHeight;
 class Player {
     constructor() {
 
-
         this.velocity = {
             x: 0,
             y: 0
@@ -29,20 +28,75 @@ class Player {
     draw() {
         //c.fillStyle = 'red'
         //c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)     
+    }
+
+    update() {
         if (this.image) {
-            c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
-        }   
+        this.draw()
+        this.position.x += this.velocity.x
+        }
     }
 }
 
 const player = new Player()
-player.draw()
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0, 0, canvas.width, canvas.height);
-    player.draw();
+    player.update();
+
+    if (keys.a.pressed) {
+        player.velocity.x = -3
+    } else if (keys.d.pressed) {
+        player.velocity.x = 3
+    } else {
+        player.velocity.x = 0
+    }
 }
 
 animate();
+
+addEventListener('keydown', ({ key }) => {
+    console.log(key)
+    switch (key) {
+        case 'a':
+            console.log('left')
+            keys.a.pressed = true
+            break;
+        case 'd':
+            console.log('right')
+            keys.d.pressed = true
+            break;
+        case ' ':
+            console.log('space')
+            break;
+    }
+})
+
+addEventListener('keyup', ({ key }) => {
+    console.log(key)
+    switch (key) {
+        case 'a':
+            keys.a.pressed = false
+            break;
+        case 'd':
+            keys.d.pressed = false
+            break;
+        case ' ':
+            console.log('space')
+            break;
+    }
+})
